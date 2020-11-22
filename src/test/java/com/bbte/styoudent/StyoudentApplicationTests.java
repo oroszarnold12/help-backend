342a3b7841c6548b43870a2420ce8e2546881fe0
Orosz Arnold- Daniel <oroszarnold12@yahoo.com>
2020-11-22 10:33:31 +0200

    basic spring project

---
+++ src/test/java/com/bbte/styoudent/StyoudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbte.styoudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyoudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
